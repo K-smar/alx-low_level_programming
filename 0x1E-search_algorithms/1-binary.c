2de663e26e1fb46a75db6faefc4d8eaad1c50590
K-smar <jessicayekini0@gmail.com>
2023-04-13 03:33:39 -0700

    1 binary

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,45 +1,61 @@
 #include "search_algos.h"
 
 /**
- * binary_search - searches for a value in an integer array using a binary
- * search algorithm, not guaranteed to return lowest index if `value` appears
- * twice in `array`
- * @array: pointer to first element of array to seach
- * @size: number of elements in array
- * @value: value to search for
+ * recursive_search - searches for a value in an array of
+ * integers using the Binary search algorithm
  *
- * Return: index containing `value`, or -1 if `value` not found or
- * `array` is NULL
+ *
+ * @array: input array
+ * @size: size of the array
+ * @value: value to search in
+ * Return: index of the number
  */
+int recursive_search(int *array, size_t size, int value)
+{
+	size_t half = size / 2;
+	size_t i;
 
+	if (array == NULL || size == 0)
+		return (-1);
+
+	printf("Searching in array");
+
+	for (i = 0; i < size; i++)
+		printf("%s %d", (i == 0) ? ":" : ",", array[i]);
+
+	printf("\n");
+
+	if (half && size % 2 == 0)
+		half--;
+
+	if (value == array[half])
+		return ((int)half);
+
+	if (value < array[half])
+		return (recursive_search(array, half, value));
+
+	half++;
+
+	return (recursive_search(array + half, size - half, value) + half);
+}
+
+/**
+ * binary_search - calls to binary_search to return
+ * the index of the number
+ *
+ * @array: input array
+ * @size: size of the array
+ * @value: value to search in
+ * Return: index of the number
+ */
 int binary_search(int *array, size_t size, int value)
 {
-	int right, mid, left;
-	int x;
+	int index;
 
-	if (array == NULL)
-	{
+	index = recursive_search(array, size, value);
+
+	if (index >= 0 && array[index] != value)
 		return (-1);
-	}
 
-	right = 0;
-	left = size - 1;
-
-	while (right <= left)
-	{
-		mid = (right + left) / 2;
-
-		printf("Searching in array: ");
-		for (x = right; x <= left; x++)
-			printf("%i%s", array[x], x == left ? "\n" : ", ");
-
-		if (array[mid] < value)
-			right = mid + 1;
-		else if (array[mid] > value)
-			left = mid - 1;
-		else
-			return (mid);
-	}
-
-	return (-1);
+	return (index);
 }
